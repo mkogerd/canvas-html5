349a6e3e87442048361e0680b400e6047111a1c9
Koger <mkogerd@gmail.com>
2018-06-26 17:52:20 -0500

    finish tutorial 2

--- canvas-resize/canvas.js
+++ canvas-resize/canvas.js
@@ -7,6 +7,7 @@ canvas.height = window.innerHeight;
 var c = canvas.getContext('2d');
 
 // Draw some rectangles
+/*
 c.fillStyle = 'rgba(0, 255, 255, 0.4)';
 c.fillRect(100, 100, 40, 40);
 c.fillStyle = 'rgba(0, 0, 255, 0.4)';
@@ -28,6 +29,12 @@ c.strokeStyle = "#0034a3";
 c.stroke();
 
 // Arc / Circle
+c.beginPath();
+c.arc(300, 300, 30, 0, Math.PI * 2, false);
+c.strokeStyle = 'blue';
+c.stroke();
+
+// Multiple Arc / Circle
 for(var i = 0; i < 40; i++) {
 	// Generate random coordinates and color
 	var x = Math.random() * window.innerWidth;
@@ -42,3 +49,59 @@ for(var i = 0; i < 40; i++) {
 	c.strokeStyle = color; 
 	c.stroke();
 }
+*/
+function Circle(x, y, dx, dy, radius, color) {
+	this.x = x;
+	this.y = y;
+	this.dx = dx;
+	this.dy = dy;
+	this.radius = radius;
+	this.color = color;
+
+	this.draw = function() {
+		c.beginPath();
+		c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+		c.strokeStyle = this.color;
+		c.fillStyle = 'rgba(25, 25, 25, .3)';
+		c.stroke();
+		//c.fill();
+	}
+
+	this.update = ()=>{
+		if (this.x + this.radius > innerWidth || this.x - this.radius < 0) 
+			this.dx = -this.dx;
+		if (this.y + this.radius > innerHeight || this.y - this.radius < 0) 
+			this.dy = -this.dy;
+		this.x += this.dx;
+		this.y += this.dy;
+		this.draw();
+	}
+}
+
+
+var circleArray = [];
+
+for (var i = 0; i < 200; i++) {
+	var radius = 30;
+	var x = Math.random() * (innerWidth - radius * 2) + radius; 
+	var y = Math.random() * (innerHeight - radius * 2) + radius;
+	var dx = (Math.random() - 0.5) * 8;
+	var dy = (Math.random() - 0.5) * 8;
+	var color = (0xFFFFFF*Math.random()<<0).toString(16);
+	while(color.length < 6) color = '0'+color;
+	color = '#'+color;
+	
+	circleArray.push(new Circle(x, y, dx, dy, radius, color));
+}
+console.log(circleArray);
+
+function animate() {
+	requestAnimationFrame(animate);
+	c.clearRect(0, 0, innerWidth, innerHeight);
+	
+	for (var i = 0; i < circleArray.length; i++) {
+		circleArray[i].update();
+	}
+}
+
+animate();
